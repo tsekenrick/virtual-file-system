b1b7abb8c8ebefd8070a8e9f1d0ab7d95191a147
Joe Versoza <jjv222@nyu.edu>
2019-03-15 07:40:29 -0400

    add revised version of FileSystem.js

---
+++ vfs/FileSystem.js
@@ -0,0 +1,7 @@
+// FileSystem.js
+// =============
+class FileSystem {
+  // initialize with object that represents virtual file system
+  // the remainder of the design of this class is up to you!
+}
+
